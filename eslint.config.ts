22fe0e7de48800b24cbcd7bf2942b4e1226fd8e9
Duane Irvin <duanei@opera.com>
2026-01-12 12:12:02 +0100

    Prevent long files

--- eslint.config.ts
+++ eslint.config.ts
@@ -21,6 +21,7 @@ export default defineConfig(
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
 
     rules: {
+      "max-lines": ["warn", 500],
       // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
       // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
       "no-undef": "off",
